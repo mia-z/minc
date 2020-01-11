fb1ffb0ceba1b21548f7d4a828a2260226c944ef
Ryan C <iperish.x@gmail.com>
2020-01-11 19:15:23 +0100

    Updates

---
+++ precache-manifest.f7c0b5d24230057a3a975b554fab30e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be2753e793e6368e307d73641d9a591",
+    "url": "/minc/index.html"
+  },
+  {
+    "revision": "f04c509a366a67381205",
+    "url": "/minc/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6f7b976d1cd167743b48",
+    "url": "/minc/static/css/main.17282391.chunk.css"
+  },
+  {
+    "revision": "f04c509a366a67381205",
+    "url": "/minc/static/js/2.9eaf775c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/minc/static/js/2.9eaf775c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f7b976d1cd167743b48",
+    "url": "/minc/static/js/main.a5505c94.chunk.js"
+  },
+  {
+    "revision": "1cce502a7e12f7c9bc67",
+    "url": "/minc/static/js/runtime-main.e75e5cc2.js"
+  }
+]);
